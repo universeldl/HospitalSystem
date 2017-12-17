2048cfceecd2681fcf265cfe9704b69c43567054
qingqing202 <qingqing202@hotmail.com>
2017-12-17 23:02:54 +0800

    template message manger
    add more function. add test case

--- hospital-wechat/src/main/java/com/hospital/wechat/service/TemplateMessageMgr.java
+++ hospital-wechat/src/main/java/com/hospital/wechat/service/TemplateMessageMgr.java
@@ -1,7 +1,7 @@
 package com.hospital.wechat.service;
 
 import com.hospital.util.WeixinUtil;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.*;
 
 /**
  * Created by QQQ on 2017/12/11.
@@ -9,7 +9,61 @@ import com.alibaba.fastjson.JSONObject;
 public class TemplateMessageMgr {
     final static String send_template_url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=ACCESS_TOKEN";
 
-    static public boolean sendTemplateMessage(JSONObject data, String openid, String redirect_url, String[] template_id, AccessTokenMgr mgr){
+    // get all template ids for current wechat
+    final static String get_template_list_url = "https://api.weixin.qq.com/cgi-bin/template/get_all_private_template?access_token=ACCESS_TOKEN";
+
+    static public boolean sendSurveyTemplate(JSONObject data, String openid, String redirect_url, AccessTokenMgr mgr) {
+        try {
+            System.out.println("sendSurveyTemplate data = " + data.toString());
+            String template_id = getSurveyTemplateId(mgr);
+            if (template_id == null) {
+                System.out.println("get survey template id failed!");
+                return false;
+            }
+            return sendTemplateMessage(data, openid, redirect_url, template_id, mgr);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    static public String getSurveyTemplateId(AccessTokenMgr mgr) {
+        try {
+            JSONObject templates = getAllTemplate(mgr);
+            JSONArray template_list = templates.getJSONArray("template_list");
+            for (int i = 0; i < template_list.size(); i++) {
+                JSONObject template = template_list.getJSONObject(i);
+                if (template.getString("title").equals("随访提醒")) {
+                    return template.getString("template_id");
+                }
+            }
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    static public JSONObject getAllTemplate(AccessTokenMgr mgr){
+        try{
+            String token = mgr.getAccessToken();
+
+            String requestUrl = get_template_list_url.replace("ACCESS_TOKEN", token);
+            JSONObject template_list = WeixinUtil.HttpsRequest(requestUrl, "GET", "");
+            if (template_list != null) {
+                System.out.println("return = "+template_list.toString());
+                return template_list;
+            } else {
+                System.out.println("get template list failed!");
+                return null;
+            }
+        }catch(Exception e ){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    static public boolean sendTemplateMessage(JSONObject data, String openid, String redirect_url, String template_id, AccessTokenMgr mgr){
         try{
 
             JSONObject f1 = new JSONObject();
